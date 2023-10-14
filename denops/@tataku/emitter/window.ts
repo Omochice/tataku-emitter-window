79dce43a13c33fc55382b991ae8aee347d2972b0
Omochice <mochice.mls.ntl@gmail.com>
2023-10-14 23:11:18 +0900

    fix: follow up to stream

--- denops/@tataku/emitter/window.ts
+++ denops/@tataku/emitter/window.ts
@@ -1,16 +1,32 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v3.8.1/function/mod.ts";
 import { execute } from "https://deno.land/x/denops_std@v3.8.1/helper/mod.ts";
-import { Emitter } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
+import {
+  assert,
+  is,
+  PredicateType,
+} from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
 
-export default class implements Emitter {
-  constructor(private readonly option: Record<string, unknown>) {
-  }
+const isOption = is.ObjectOf({
+  cmd: is.String,
+});
 
-  run(denops: Denops, source: string[]) {
-    await execute(denops, `${option.cmd ?? "enew"}`);
-    const bufnr = await fn.bufnr(denops);
-    await fn.setbufvar(denops, bufnr, "&buftype", "nofile");
-    await fn.setbufline(denops, bufnr, 1, source);
-  }
-}
+type Option = PredicateType<typeof isOption>;
+
+const defaultOption: Option = {
+  cmd: "enew",
+};
+
+const emitter = (denops: Denops, option = defaultOption) => {
+  assert(option, isOption);
+  return new WritableStream<string[]>({
+    write: async (chunk: string[]) => {
+      await execute(denops, option?.cmd ?? defaultOption.cmd);
+      const bufnr = await fn.bufnr(denops);
+      await fn.setbufvar(denops, bufnr, "&buftype", "nofile");
+      await fn.setbufline(denops, bufnr, 1, chunk);
+    },
+  });
+};
+
+export default emitter;
